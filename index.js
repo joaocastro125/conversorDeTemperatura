2f77e5e5e37ec8ea1f4e153696ab182924007269
joao castro <joaocastro_125@hotmail.com>
2023-01-31 20:45:01 -0300

    update project

---
+++ index.js
@@ -0,0 +1,12 @@
+let celcus=30
+
+let fahrenheit=celcus* 9 / 5 + 32;
+
+
+let Tempfahrenheit=40
+let Tempcelsus=((celcus-32)/1.8).toFixed(3)
+
+
+
+
+alert("a minha temperatura em celsus é :"+Tempcelsus)
